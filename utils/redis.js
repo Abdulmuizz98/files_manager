da4cef00c76e6b324cac91b9a4ef84f92e8effc5
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-06 23:43:09 +0100

    feat: implement redisClient

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.get = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (err) => console.log('Redis Client Error', err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    let value;
+    try {
+      value = await this.get(key);
+    } catch (err) {
+      console.log(err);
+    }
+    return value;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+const redisClient = new RedisClient();
+export default redisClient;
